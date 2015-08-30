6f1e218bd0f7d63f7f8eebb346dc644775ce2e2f
David Paluy <david@paluy.org>
2015-08-30 12:56:45 +0300

    fix assets url

--- lib/trumbowyg_rails.rb
+++ lib/trumbowyg_rails.rb
@@ -1,4 +1,11 @@
 module TrumbowygRails
   class Engine < Rails::Engine
+    initializer "trumbowyg.assets.precompile" do |app|
+      app.config.assets.paths << Rails.root.join("vendor", "assets", "images", "trumbowyg")
+      app.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts", "trumbowyg")
+      app.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "trumbowyg")
+      app.config.assets.precompile += %w(*.png *.js )
+      app.config.assets.precompile << /.+\.css$/
+    end
   end
 end
